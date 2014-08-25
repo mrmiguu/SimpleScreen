4e3384858d67cea15e0e0c29c1def035f351dac4
mrmiguu <alexander.w.matulionis@gmail.com>
2014-08-24 20:26:44 -0700

    Things like bip & cap

--- com/mrmiguu/SimpleScreen.java
+++ com/mrmiguu/SimpleScreen.java
@@ -19,7 +19,9 @@ import java.util.List;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
-public final class SimpleScreen {
+public final class SimpleScreen
+    implements AutoCloseable {
+
   public final int
     width,
     height,
@@ -32,9 +34,11 @@ public final class SimpleScreen {
     DEFAULT_BUFFER_COUNT = 2,
     DEFAULT_IMAGE_FILE_COUNT = 128,
     DEFAULT_PICTURE_COUNT = 512,
-    DEFAULT_SURFACE_COUNT = 16;
+    DEFAULT_SURFACE_COUNT = 16,
+    DEFAULT_STATIC_ANIMATION_COUNT = 128,
+    DEFAULT_DYNAMIC_ANIMATION_COUNT = 512;
   private static final String
-    DEFAULT_TITLE = "";
+    DEFAULT_TITLE = "SimpleScreen";
 
   private final BufferStrategy
     bufferStrategy;
@@ -48,6 +52,12 @@ public final class SimpleScreen {
     pictures;
   private final Surface[]
     surfaces;
+  private final StaticAnimation[]
+    staticAnimations;
+  private final DynamicAnimation[]
+    dynamicAnimations;
+  private final Thread
+    animationTimer; // for looping static animations via their intervals
 
   private Graphics
     graphics;
@@ -63,13 +73,25 @@ public final class SimpleScreen {
   }
   //````````````````````````````````````````````````````````````````````````````````````````````````
 
+  private static <T> boolean isEmpty(final T[] array) {
+    for (int i = array.length; --i >= 0;) {
+      if (array[i] == null) continue;
+      else return false;
+    }
+
+    return true;
+  }
+  //````````````````````````````````````````````````````````````````````````````````````````````````
+
   public SimpleScreen(final String title,
       final Dimension size,
       final int resolution,
       final int bufferCount,
       final int imageFileCount,
       final int pictureCount,
-      final int surfaceCount) {
+      final int surfaceCount,
+      final int staticAnimationCount,
+      final int dynamicAnimationCount) {
 
     final Dimension
       size_ = (size == null ? new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT) : size);
@@ -104,6 +126,42 @@ public final class SimpleScreen {
     imageFiles = new ImageFile[imageFileCount == -1 ? DEFAULT_IMAGE_FILE_COUNT : imageFileCount];
     pictures = new Picture[pictureCount == -1 ? DEFAULT_PICTURE_COUNT : pictureCount];
     surfaces = new Surface[surfaceCount == -1 ? DEFAULT_SURFACE_COUNT : surfaceCount];
+    staticAnimations = new StaticAnimation[staticAnimationCount == -1
+      ? DEFAULT_STATIC_ANIMATION_COUNT
+      : staticAnimationCount];
+    dynamicAnimations = new DynamicAnimation[dynamicAnimationCount == -1
+      ? DEFAULT_DYNAMIC_ANIMATION_COUNT
+      : dynamicAnimationCount];
+
+    /*
+     * Handles the loop for occurring animations
+     */
+    (animationTimer = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          for (;;) {
+            final long
+              time = System.currentTimeMillis();
+
+            for (StaticAnimation s : staticAnimations) {
+              if (s != null) {
+                if (s.timer < time) {
+                  s.frame = (s.frame + 1) % s.images.length;
+                  s.timer = time + s.interval;
+                }
+              }
+            }
+
+            animationTimer.sleep(15); // nothing should animate fast than 60 fps
+          }
+        }
+        catch (final InterruptedException e) {
+          animationTimer.interrupt();
+          e.printStackTrace();
+        }
+      }
+    })).start();
   }
   //````````````````````````````````````````````````````````````````````````````````````````````````
 
@@ -133,13 +191,12 @@ public final class SimpleScreen {
 
     /*
      * Remove resolution scaling code and scale images when saving them after creating them
+     * (all I have to do now is store the pictures in relation to the resolution)
      */
     graphics.drawImage(
       pictures[index].image,
       (pictures[index].location.x + pictures[index].xOffset) << resolutionBit,
       (pictures[index].location.y + pictures[index].yOffset) << resolutionBit,
-      pictures[index].image.getWidth() << resolutionBit,
-      pictures[index].image.getHeight() << resolutionBit,
       null);
   }
   //````````````````````````````````````````````````````````````````````````````````````````````````
@@ -149,13 +206,33 @@ public final class SimpleScreen {
 
     /*
      * Remove resolution scaling code and scale images when saving them after creating them
+     * (all I have to do now is store the surfaces in relation to the resolution)
      */
     graphics.drawImage(
       surfaces[index].image,
       surfaces[index].location.x << resolutionBit,
       surfaces[index].location.y << resolutionBit,
-      surfaces[index].image.getWidth() << resolutionBit,
-      surfaces[index].image.getHeight() << resolutionBit,
+      null);
+  }
+  //````````````````````````````````````````````````````````````````````````````````````````````````
+
+  public void drawDynamicAnimation(final int index)
+      throws IndexOutOfBoundsException {
+
+    drawStaticAnimation(dynamicAnimations[index].animations[dynamicAnimations[index].state[0]]);
+  }
+  //````````````````````````````````````````````````````````````````````````````````````````````````
+
+  public void drawStaticAnimation(final int index)
+      throws IndexOutOfBoundsException {
+
+    final StaticAnimation
+      staticAnim = staticAnimations[index];
+
+    graphics.drawImage(
+      staticAnim.images[staticAnim.frame],
+      (staticAnim.location.x + staticAnim.xOffset) << resolutionBit,
+      (staticAnim.location.y + staticAnim.yOffset) << resolutionBit,
       null);
   }
   //````````````````````````````````````````````````````````````````````````````````````````````````
@@ -202,12 +279,60 @@ public final class SimpleScreen {
   }
   //````````````````````````````````````````````````````````````````````````````````````````````````
 
+  public int createStaticAnimation(final int imageFileIndex,
+        final Rectangle[] frames,
+        final int interval,
+        final Point location,
+        final Point offset)
+            throws IndexOutOfBoundsException {
+
+    final int
+      index = getOpenIndex(staticAnimations);
+    staticAnimations[index] = new StaticAnimation(
+      imageFileIndex,
+      frames,
+      interval,
+      location,
+      offset);
+
+    return index;
+  }
+  //````````````````````````````````````````````````````````````````````````````````````````````````
+
+  public int createDynamicAnimation(final int[] staticAnimIndices,
+        final int[] state)
+            throws IndexOutOfBoundsException {
+
+    final int
+      index = getOpenIndex(dynamicAnimations);
+    dynamicAnimations[index] = new DynamicAnimation(staticAnimIndices, state);
+
+    return index;
+  }
+  //````````````````````````````````````````````````````````````````````````````````````````````````
+
   public void setPictureLocation(final int index,
       final Point location)
           throws IndexOutOfBoundsException {
 
     pictures[index].location = location;
   }
+  //````````````````````````````````````````````````````````````````````````````````````````````````
+
+  public void setStaticAnimationLocation(final int index,
+      final Point location)
+          throws IndexOutOfBoundsException {
+
+    staticAnimations[index].location = location;
+  }
+  //````````````````````````````````````````````````````````````````````````````````````````````````
+
+  // public void setDynamicAnimationLocation(final int index,
+  //     final Point location)
+  //         throws IndexOutOfBoundsException {
+
+  //   dynamicAnimations[index].location = location;
+  // }
   //````````````````````````````````````````````````````````````````````````````````````````````````
 
   public int imageFileCount() {
@@ -226,13 +351,34 @@ public final class SimpleScreen {
 
     if (surfaces[surfaceIndex] != null) {
       surfaces[surfaceIndex].addPicture(pictureIndex);
+      pictures[pictureIndex].dispose();
       pictures[pictureIndex] = null;
     }
   }
   //````````````````````````````````````````````````````````````````````````````````````````````````
 
-  public void dispose() {
-    for (Picture p : pictures) if (p != null) p.dispose();
+  @Override
+  public void close()
+      throws Exception {
+
+    // stop our animations and wait for them to stop
+    animationTimer.interrupt();
+    animationTimer.join();
+
+    for (int i = imageFiles.length; --i >= 0;) imageFiles[i] = null;
+    for (int i = pictures.length; --i >= 0;) {
+      if (pictures[i] != null) pictures[i].dispose();
+      pictures[i] = null;
+    }
+    for (int i = surfaces.length; --i >= 0;) {
+      if (surfaces[i] != null) surfaces[i].dispose();
+      surfaces[i] = null;
+    }
+    for (int i = staticAnimations.length; --i >= 0;) {
+      if (staticAnimations[i] != null) staticAnimations[i].dispose();
+      staticAnimations[i] = null;
+    }
+    for (int i = dynamicAnimations.length; --i >= 0;) dynamicAnimations[i] = null;
   }
   //````````````````````````````````````````````````````````````````````````````````````````````````
 
@@ -255,7 +401,7 @@ public final class SimpleScreen {
   //````````````````````````````````````````````````````````````````````````````````````````````````
 
   /**
-   * The actual image drawn to the screen (there should be as few of these as possible).
+   * The actual image drawn to the screen or an individual surface.
    */
   private final class Picture {
     public final int
@@ -285,19 +431,25 @@ public final class SimpleScreen {
             imageFiles[imageFileIndex].image.getHeight())
           : frame);
 
-      image = new BufferedImage(frame_.width, frame_.height, BufferedImage.TYPE_INT_ARGB);
+      // image = new BufferedImage(frame_.width, frame_.height, BufferedImage.TYPE_INT_ARGB);
 
-      image.getGraphics().drawImage(
-        imageFiles[imageFileIndex].image,
-        0,
-        0,
-        image.getWidth(),
-        image.getHeight(),
+      image = imageFiles[imageFileIndex].image.getSubimage(
         frame_.x,
         frame_.y,
-        frame_.x + frame_.width,
-        frame_.y + frame_.height,
-        null);
+        frame_.width,
+        frame_.height);
+
+      // image.getGraphics().drawImage(
+      //   imageFiles[imageFileIndex].image,
+      //   0,
+      //   0,
+      //   image.getWidth(),
+      //   image.getHeight(),
+      //   frame_.x,
+      //   frame_.y,
+      //   frame_.x + frame_.width,
+      //   frame_.y + frame_.height,
+      //   null);
 
       final Point
         offset_ = (offset == null ? new Point() : offset);
@@ -313,6 +465,9 @@ public final class SimpleScreen {
   }
   //````````````````````````````````````````````````````````````````````````````````````````````````
 
+  /**
+   * An open field for drawing non-moving pictures onto.
+   */
   private final class Surface {
     public final BufferedImage
       image;
@@ -356,6 +511,80 @@ public final class SimpleScreen {
 
     public void dispose() {
       location = null;
+    }
+  }
+  //````````````````````````````````````````````````````````````````````````````````````````````````
+
+  /**
+   * An array of images that change at an interval.
+   */
+  private final class StaticAnimation {
+    public final BufferedImage[]
+      images;
+    public final int
+      interval,
+      xOffset,
+      yOffset;
+
+    public Point
+      location; // references an ever-changing point
+    public long
+      timer;
+    public int
+      frame;
+    //``````````````````````````````````````````````````````````````````````````````````````````````
+
+    public StaticAnimation(final int imageFileIndex,
+        final Rectangle[] frames,
+        final int interval,
+        final Point location,
+        final Point offset)
+            throws IndexOutOfBoundsException {
+
+      images = new BufferedImage[frames.length];
+
+      for (int i = 0; i < images.length; ++i) {
+        images[i] = imageFiles[imageFileIndex].image.getSubimage(
+          frames[i].x,
+          frames[i].y,
+          frames[i].width,
+          frames[i].height);
+      }
+
+      this.interval = interval;
+      this.location = (location == null ? new Point() : location);
+
+      final Point
+        offset_ = (offset == null ? new Point() : offset);
+      xOffset = offset_.x;
+      yOffset = offset_.y;
+    }
+    //``````````````````````````````````````````````````````````````````````````````````````````````
+
+    public void dispose() {
+      for (int i = images.length; --i >= 0;) images[i] = null;
+      location = null;
+    }
+  }
+  //````````````````````````````````````````````````````````````````````````````````````````````````
+
+  /**
+   * An array of static animations that change depending on a state reference.
+   */
+  private final class DynamicAnimation {
+    public final int[]
+      animations,
+      state;
+
+    // public Point
+    //   location; // references an ever-changing point
+    //``````````````````````````````````````````````````````````````````````````````````````````````
+
+    public DynamicAnimation(final int[] staticAnimIndices,
+        final int[] state) {
+
+      animations = staticAnimIndices;
+      this.state = state;
     }
   }
 }
